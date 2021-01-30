2ac22875cb689bbdc4f0b6c1c5cc8171801d2e15
Morse579 <yushan579@gmail.com>
2021-01-29 23:26:29 -0800

    delete some packages
    delete some packages

--- milestone_1/src/antlr/XPath.java
+++ milestone_1/src/antlr/XPath.java
@@ -1,7 +1,6 @@
 package antlr;
 
 import java.nio.file.*;
-import java.io.File;
 import java.nio.charset.*;
 import java.io.IOException;
 import org.antlr.v4.runtime.*;
@@ -10,8 +9,6 @@ import org.w3c.dom.Node;
 import java.util.List;
 import java.util.ArrayList;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 
 
 public class XPath {
